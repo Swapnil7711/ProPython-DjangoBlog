cf72146e89a805426cebf4e683a4c5f33cdcde32
SWAPNIL HARPALE <swapnilharpale@SWAPNILs-MacBook-Air.local>
2019-12-20 18:50:38 +0530

    added contact model

--- ProPython/blog/models.py
+++ ProPython/blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Contact(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    email = models.CharField(max_length=100)
+    description = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True, blank=True)
+
